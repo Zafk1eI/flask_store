020b452a8e0e2e94e2f334d2d9f5e15b7bdcd2b0
Zafk1eI <sintfrank@gmail.com>
2024-04-30 01:37:13 +0300

    update readme

--- config_example.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = 'SECRET_KEY'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'mysql://user:password@localhost/db_name')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_ADMIN_SWATCH = 'cerulean'
